3f989e5eb1185a3b52f829b15413ef2f93ccb4ea
Dan Mehlqvist <dan.mehlqvist@gmail.com>
2018-03-16 22:41:21 +0100

    First commit

---
+++ src/webparts/userListing/components/IUserListingProps.ts
@@ -0,0 +1,3 @@
+export interface IUserListingProps {
+  description: string;
+}
---
+++ src/webparts/userListing/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IUserListingWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'UserListingWebPartStrings' {
+  const strings: IUserListingWebPartStrings;
+  export = strings;
+}
